05192ae1b04b9ec3c4b2168f8c0d91445efc765b
Magda98 <magdakochman7@gmail.com>
2020-10-10 23:29:41 +0200

    🐛 sidebar fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -100,7 +100,6 @@ h4 {
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  max-width: fit-content;
   height: 100vh;
   align-items: center;
   justify-content: center;
