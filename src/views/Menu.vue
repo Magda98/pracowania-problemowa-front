a3956ff0e52a769db3c1b5057b73d00cc4de4602
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2020-10-14 18:43:40 +0200

    Menu v2

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -7,43 +7,52 @@
       backgroundImage: 'url(' + require('../assets/bg-1_white.png') + ')'
     }"
   >
+    <v-container :style="{ top: '70px', position: 'absolute' }">
       <v-row justify="center">
         <h1>Oferta:</h1>
         <br />
       </v-row>
-    <v-container>
       <v-row justify="center">
-        <v-tabs v-model="tab" centered>
+        <v-tabs v-model="tab" centered background-color="light-green accent-1">
           <v-tabs-slider color="black" />
           <v-tab v-for="day in days" :key="day">
             {{ day }}
           </v-tab>
         </v-tabs>
         <br />
-        <v-tabs-items v-model="tab">
-          <v-tab-item v-for="day in days" :key="day">
-            <v-card flat>
-              <v-card-title>
-                Jadłospis
-                <v-spacer></v-spacer>
-                <v-text-field
-                  v-model="search"
-                  append-icon="mdi-magnify"
-                  label="Wyszukaj"
-                  single-line
-                  hide-details
-                ></v-text-field>
-              </v-card-title>
-              <v-data-table
-                :headers="headers"
-                :items="_dishes(day)"
-                :search="search"
-                multi-sort
-                hide-default-footer
-              />
-            </v-card>
-          </v-tab-item>
-        </v-tabs-items>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col md="10">
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="day in days" :key="day">
+              <v-card elevation="3">
+                <v-card-title>
+                  Jadłospis
+                  <v-spacer></v-spacer>
+                  <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Wyszukaj"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+                <v-data-table
+                  v-model="selected"
+                  :headers="headers"
+                  :items="_dishes(day)"
+                  :search="search"
+                  show-select
+                  multi-sort
+                  hide-default-footer
+                />
+              </v-card>
+            </v-tab-item> </v-tabs-items
+          ><br />
+          <v-btn block elevation="3" color="light-green accent-1" @click="order"
+            >Dodaj do zamówienia</v-btn
+          >
+        </v-col>
       </v-row>
     </v-container>
   </v-container>
@@ -56,12 +65,24 @@ export default {
   methods: {
     _dishes(value) {
       return this.dishes.filter(dishes => dishes.day == value);
+    },
+
+    order() {
+      var ids = [];
+      this.selected.forEach(addid);
+
+      function addid(item) {
+        ids.push(item.id);
+      }
+
+      alert("Do zamówienia dodano posiłki o id: " + ids);
     }
   },
   data() {
     return {
       tab: null,
       search: "",
+      selected: [],
       days: [
         "Poniedziałek",
         "Wtorek",
