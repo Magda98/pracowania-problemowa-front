9b1038aba3d65fcaf4331568119b4f4969870e2b
Magda98 <magdakochman7@gmail.com>
2020-11-04 23:18:15 +0100

    🚀

--- src/router/index.js
+++ src/router/index.js
@@ -56,8 +56,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  // base: process.env.BASE_URL,
-  base: "/pracowania-problemowa-front/",
+  base: process.env.BASE_URL,
   routes
 });
 
