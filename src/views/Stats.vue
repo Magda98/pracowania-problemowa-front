fe6023d680429a5b739e34ec23fa53f53c467440
Kamil Król <kamil.krol.1997@gmail.com>
2020-11-30 01:37:45 +0100

    🎉 Stats

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -4,7 +4,7 @@
     fill-height
     fluid
     :style="{
-      backgroundImage: 'url(' + require('../assets/bg-1_white.png') + ')'
+      backgroundImage: 'url(' + require('../assets/bg-1_white.png') + ')',
     }"
   >
     <v-card :style="{ top: '70px', position: 'absolute', width: '98%' }">
@@ -20,18 +20,29 @@
         ></v-text-field>
       </v-card-title>
       <v-data-table
-        v-if="true"
-        :headers="headers"
+        v-if="tableData"
+        :headers="getHeaders(showAll)"
         :search="search"
-        :items="summary"
+        :items="getData(showAll)"
         :footer-props="{
           'items-per-page-text': 'Wierszy na stronę',
           'items-per-page-all-text': 'Wszystkie',
-          'items-per-page-options': [10, 25, 50, -1]
+          'items-per-page-options': [10, 25, 50, -1],
         }"
         class="elevation-1"
         multi-sort
       >
+        <template v-slot:item.day="{ item }">
+          <span>{{ days[item.day] }}</span>
+        </template>
+        <template v-slot:top>
+          <v-switch
+            v-model="showAll"
+            label="Widok szczegółowy"
+            inset
+            class="pa-3"
+          ></v-switch>
+        </template>
       </v-data-table>
       <div
         v-else
@@ -50,7 +61,9 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   data() {
     return {
+      showAll: false,
       search: "",
+      tableData: [],
       days: [
         "Poniedziałek",
         "Wtorek",
@@ -58,54 +71,134 @@ export default {
         "Czwartek",
         "Piątek",
         "Sobota",
-        "Niedziela"
+        "Niedziela",
       ],
       tab: null,
       headers: [
         {
+          text: "Dzień",
+          align: "start",
+          value: "day",
+        },
+        {
           text: "Posiłek",
           align: "start",
-          value: "offerName"
+          value: "offerName",
         },
         {
           text: "Instytucja",
           align: "start",
-          value: "institution"
+          value: "institution",
         },
         {
           text: "Ilość",
           align: "start",
-          value: "ammount"
-        }
-      ]
+          value: "ammount",
+        },
+      ],
     };
   },
   computed: {
     ...mapGetters("offers", ["offersList"]),
     ...mapGetters("institutions", ["institutionsList"]),
-    ...mapGetters("orders", ["summaryInstitution", "summary", "ordersList"])
+    ...mapGetters("orders", ["summaryInstitution", "summary", "ordersList"]),
   },
   methods: {
     ...mapActions("offers", ["getOffers"]),
     ...mapActions("orders", [
       "getOrder",
       "getSummaryInstitution",
-      "getSummary"
+      "getSummary",
     ]),
-    ...mapActions("institutions", ["getInstitutions"])
+    ...mapActions("institutions", ["getInstitutions"]),
+    // @vuese
+    // Funkcja zwraca odpowiednie nagłówki w zależności od stanu przełącznika
+    // @arg argumentem jest stan przełącznika
+    getHeaders(val) {
+      if (!val) {
+        return [
+          {
+            text: "Posiłek",
+            align: "start",
+            value: "offerName",
+          },
+          {
+            text: "Ilość",
+            align: "start",
+            value: "ammount",
+          },
+        ];
+      } else {
+        return [
+          {
+            text: "Dzień",
+            align: "start",
+            value: "day",
+          },
+          {
+            text: "Posiłek",
+            align: "start",
+            value: "offerName",
+          },
+          {
+            text: "Instytucja",
+            align: "start",
+            value: "institution",
+          },
+          {
+            text: "Ilość",
+            align: "start",
+            value: "ammount",
+          },
+        ];
+      }
+    },
+    // @vuese
+    // Funkcja zwraca odpowiednie dane w zależności od stanu przełącznika
+    // @arg argumentem jest stan przełącznika
+    getData(val) {
+      if (!val) {
+        return this.summary;
+      } else {
+        return this.tableData;
+      }
+    },
   },
   async beforeMount() {
     this.getSummary();
     this.getInstitutions();
+    this.getOffers();
     let module = this;
+    let ids = [];
+    let index = 0;
+    let i;
+    let instName;
+    let day;
     const myArray = await Promise.all(
-      this.institutionsList.map(function(item) {
-        console.log(item.publicId);
+      this.institutionsList.map(function (item) {
+        ids.push(item.publicId);
         return module.getSummaryInstitution(item.publicId);
       })
     );
-    console.log(myArray);
-  }
+    myArray.forEach(fill1);
+    function fill1(item) {
+      for (i = 0; i < item.length; i++) {
+        instName = module.institutionsList.find((obj) => {
+          return obj.publicId === ids[index];
+        }).name;
+        day = module.offersList.find((obj) => {
+          return obj.publicId === item[i].offerId;
+        }).dayOfWeek;
+        module.tableData.push({
+          day: day,
+          offerName: item[i].offerName,
+          institution: instName,
+          ammount: item[i].ammount,
+        });
+      }
+      index++;
+    }
+  },
 };
 </script>
 <style lang="scss" scoped>
